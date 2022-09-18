bca3fc73c9d006f0efb38db2e3d0bb16a0e4bf5e
tsukudamayo <tsukudamayo@gmail.com>
2022-09-18 03:34:26 +0000

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()    
 setup(
     name="tsukuda_fib_py",
-    version="0.0.1",
+    version="0.0.0",
     author="tsukudamayo",
     author_email="tsukudamayo@gmail.com",
     description="Calculates a FIbonacci number",
@@ -17,8 +17,8 @@ setup(
         "dill>=0.2.8",
     ],
     extras_require={
-        "server": ["Flask>=1.0.0"],
-    }
+        "server": ["Flask>=1.0.0"]
+    },
     packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta",
