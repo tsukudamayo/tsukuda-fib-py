c0194c8a80a0d99c3b365a4d7fc7a6033855a074
tsukudamayo <tsukudamayo@gmail.com>
2022-09-18 12:04:56 +0900

    Setting up type checking for Python

--- setup.py
+++ setup.py
@@ -12,7 +12,13 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tsukudamayo/tsukuda-fib-py",
-    install_requires=[],
+    install_requires=[
+        "PyYAML>=4.1.2",
+        "dill>=0.2.8",
+    ],
+    extras_require={
+        "server": ["Flask>=1.0.0"],
+    }
     packages=find_packages(exclude=("tests",)),
     classifiers=[
         "Development Status :: 4 - Beta",
